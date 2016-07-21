7eebca659c03bcd99f51f6d6b35ede62d791d1cf
Eric <eric.wenbl@gmail.com>
2016-07-21 17:00:09 +0800

    add new

--- _posts/2016-07-16-sublime-tips.markdown
+++ _posts/2016-07-16-sublime-tips.markdown
@@ -4,5 +4,13 @@ layout: post
 tags: Sublime
 ---
 
-# 如何在不同电脑上同步 `sublime` 的配置文件？
+## 如何在不同电脑上同步 `sublime` 的配置文件？
 
+## 主要插件及其设置
+
+- Package Controll
+- GitGutter 
+- Git
+- Jekyll
+- Anaconda
+- SublimeREPL
--- analysis/study.py
+++ analysis/study.py
@@ -2,9 +2,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-s = pd.Series([1,3,5,np.nan,6,8])
-print(s)
-
-import tushare as ts
-
-print(ts.sh_margins(start='2015-01-01', end='2015-04-19'))
+df = pd.read_excel("./banks.xls")
+print(df.head())
